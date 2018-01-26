74e25f33b2db3e7a95d885646a3f2adc61f3143a
Josh-Stillman <joshstillman@gmail.com>
2018-01-26 11:42:34 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,22 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state
+
+  function getState(){
+    return reducer(state, "ASDF")
+  }
+  function dispatch(action){
+    state = reducer(state, action)
+    render()
+  }
+
+
+
+
+  return {getState, dispatch}
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
+//createStore(countReducer).dispatch("@@INIT")
